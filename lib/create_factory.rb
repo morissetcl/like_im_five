d2d4bd41092ece99f1fb522b4981c0e943669c3c
Clement Morisset <morissetcl87@gmail.com>
2019-12-30 23:42:30 +0100

    generate a txt file with formatted factories

---
+++ lib/create_factory.rb
@@ -0,0 +1,23 @@
+require 'rails'
+
+class CreateFactory
+  class << self
+    def call(objects)
+      File.open("spec/data.txt", "w+") do |f|
+        objects.each do |object|
+          f.puts(formatte_factory(object))
+          f.puts("\n")
+        end
+      end
+    end
+
+    private
+
+    def formatte_factory(object)
+      atttributes_symbolized = object[:attributes][0].inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+      formatted_attributes = atttributes_symbolized.to_s.delete(':').delete('{').delete('}').gsub("=>", ": ")
+      factory_name = object[:table].singularize.to_sym
+      "let(:#{factory_name}) { create :#{factory_name}, #{formatted_attributes} }"
+    end
+  end
+end
