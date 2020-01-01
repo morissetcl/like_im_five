293488437bdb56e312d8d1559ee2d3b0e53447c3
Clément MORISSET <cmorisset@captive.fr>
2020-01-01 10:12:54 +0100

    refactore extract_associated_object.rb

--- lib/extract_associated_object.rb
+++ lib/extract_associated_object.rb
@@ -3,11 +3,12 @@ require "rubygems/text"
 require_relative './create_factory'
 
 class ExtractAssociatedObject
-  attr_reader :object, :associated_object
+  attr_reader :object, :associated_object, :schema_tables
 
   def initialize(object, associated_object = [])
     @object = object[0]
     @associated_object = associated_object
+    @schema_tables = ActiveRecord::Base.connection.tables
   end
 
   def call
@@ -30,20 +31,27 @@ class ExtractAssociatedObject
     tables_names = []
     object.select do |column|
       next unless column.include?("_id")
+
       table = column.split("_id").join.pluralize
-      table_exist = ActiveRecord::Base.connection.tables.include?(table)
-      if !table_exist
-        ActiveRecord::Base.connection.tables.each do |table_name|
-           levenshtein = Class.new.extend(Gem::Text).method(:levenshtein_distance)
-           similarity = levenshtein.call(table, table_name)
-           next if similarity > 3
-
-           table = table_name
-        end
+      if table_not_exist?(table)
+        table = find_closest_table_name(column, table)
       end
-
       tables_names << ["#{table}", column]
     end
     tables_names
+  end
+
+  def table_not_exist?(table)
+    !schema_tables.include?(table)
+  end
+
+  def find_closest_table_name(column, table)
+    schema_tables.each do |table_name|
+       levenshtein = Class.new.extend(Gem::Text).method(:levenshtein_distance)
+       similarity = levenshtein.call(table, table_name)
+       next if similarity > 3
+
+       return table_name
+    end
   end
 end
