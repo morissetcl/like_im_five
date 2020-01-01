7ef74f66e948769a471ac2d58082b07ab7744d65
Clément MORISSET <cmorisset@captive.fr>
2020-01-01 10:44:34 +0100

    refactore extract_specific_object.rb

--- lib/extract_specific_object.rb
+++ lib/extract_specific_object.rb
@@ -6,20 +6,27 @@ require_relative './extract_associated_object'
 class ExtractSpecificObject
   class << self
     def call(id:, model:)
-      db_config = YAML.load(ERB.new(File.read('./config/database.yml')).result)
-      ActiveRecord::Base.establish_connection(db_config['development'])
+      initialize_configuration
       return table_not_exist(model) unless ActiveRecord::Base.connection.tables.include?(model)
 
       result = ActiveRecord::Base.connection.execute("SELECT * FROM #{model} WHERE id='#{id}'").to_a
       return not_found(model, id) if result.blank?
 
-      result[0].delete('updated_at')
-      result[0].delete('created_at')
-
+      remove_timestamps(result)
       ExtractAssociatedObject.new(result).call
     end
 
     private
+
+    def remove_timestamps(result)
+      result[0].delete('updated_at')
+      result[0].delete('created_at')
+    end
+
+    def initialize_configuration
+      db_config = YAML.load(ERB.new(File.read('./config/database.yml')).result)
+      ActiveRecord::Base.establish_connection(db_config['development'])
+    end
 
     def not_found(model, id)
       print "Can't find id=#{id} in table=#{model}"
